6a600a489a3f9801cfe28ca7d8a263ca2d747d88
anthonyduong <anthonyduong9@gmail.com>
2025-03-23 23:46:06 -0700

    adds type checking to Python apps

--- apps/autointerp/neuronpedia_autointerp/routes/score/fuzz_detection.py
+++ apps/autointerp/neuronpedia_autointerp/routes/score/fuzz_detection.py
@@ -2,6 +2,10 @@ import traceback
 
 import torch
 from fastapi import HTTPException
+from neuronpedia_autointerp.utils import (
+    convert_classifier_output_to_score_classifier_output,
+    per_feature_scores_fuzz_detection,
+)
 from neuronpedia_autointerp_client.models.np_score_fuzz_detection_type import (
     NPScoreFuzzDetectionType,
 )
@@ -15,11 +19,6 @@ from sae_auto_interp.clients import OpenRouter
 from sae_auto_interp.features import Example, Feature, FeatureRecord
 from sae_auto_interp.scorers import DetectionScorer, FuzzingScorer
 from sae_auto_interp.scorers.scorer import ScorerResult
-
-from neuronpedia_autointerp.utils import (
-    convert_classifier_output_to_score_classifier_output,
-    per_feature_scores_fuzz_detection,
-)
 
 
 async def generate_score_fuzz_detection(request: ScoreFuzzDetectionPostRequest):
