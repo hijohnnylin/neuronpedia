0be0945e340ee74fc8b24db84b549aa1341c0e2c
anthonyduong <anthonyduong9@gmail.com>
2025-03-31 23:56:03 -0700

    makes changes to upload coverage reports to Codecov

--- apps/webapp/vitest.config.ts
+++ apps/webapp/vitest.config.ts
@@ -1,25 +1,27 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { loadEnvConfig } from '@next/env';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
 loadEnvConfig(process.cwd());
 
-export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
-  test: {
-    // https://github.com/vitest-dev/vitest/issues/990
-    globals: true,
-    include: ['components/**/*.test.tsx'],
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
-    deps: {
-      inline: ['vitest-canvas-mock'],
-    },
-    environmentOptions: {
-      jsdom: {
-        resources: 'usable',
+export default defineConfig(async () => {
+  const { default: tsconfigPaths } = await import('vite-tsconfig-paths');
+  return {
+    plugins: [tsconfigPaths(), react()],
+    test: {
+      // https://github.com/vitest-dev/vitest/issues/990
+      globals: true,
+      include: ['components/**/*.test.tsx'],
+      environment: 'jsdom',
+      setupFiles: ['./vitest.setup.ts'],
+      deps: {
+        inline: ['vitest-canvas-mock'],
+      },
+      environmentOptions: {
+        jsdom: {
+          resources: 'usable',
+        },
       },
     },
     coverage: {
